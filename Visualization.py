e7206a376e8b8912f1935b18c21f26a75b374432
klaudiaf24 <klaudia.fil@gmail.com>
2020-11-28 21:11:38 +0100

    [CHPP_IMP_02] Add basic drawing function

--- Visualization.py
+++ Visualization.py
@@ -2,11 +2,61 @@
 
 import matplotlib.pyplot as plt
 import networkx as nx
-import random
-
 from networkx.drawing.nx_agraph import graphviz_layout
-
+import random
 
 
 def draw(graph, route=[]):
-   pass
+    weight = nx.get_edge_attributes(graph, 'weight')
+    address = nx.get_node_attributes(graph, 'address')
+    streetName = nx.get_edge_attributes(graph, 'streetName')
+
+    position = nx.get_node_attributes(graph, 'pos')
+    if not position:
+        position = nx.spring_layout(graph, k=2, iterations=20, seed=2)
+        
+    # Houses nodes - grey
+    # Post office - red 
+    # Crossings - yellow
+    if address:
+        for idx in graph.nodes:
+            graph.nodes[idx]['color'] = 'green' if graph.nodes[idx]['streetName'] == "" else 'grey'
+            graph.nodes[idx]['size'] = 50 if graph.nodes[idx]['streetName'] == "" else 200
+            if graph.nodes[idx]['isPostOffice'] == True:
+                graph.nodes[idx]['color'] = 'red'
+
+
+        colors = [node[1]['color'] for node in graph.nodes(data=True)]
+        size = [node[1]['size'] for node in graph.nodes(data=True)]
+
+    else:
+        for idx in graph.nodes:
+            graph.nodes[idx]['color'] = 'red' if graph.nodes[idx]['isPostOffice'] == True else 'grey'
+            graph.nodes[idx]['size'] = 200
+
+        colors = [node[1]['color'] for node in graph.nodes(data=True)]
+        size = [node[1]['size'] for node in graph.nodes(data=True)]
+
+    nx.draw_networkx_nodes(graph, position, node_size=size, node_color=colors)
+
+    # highlight
+    nx.draw_networkx_edges(graph, position, arrows=True, width=2.5, edgelist=route, edge_color='red')
+
+    # non highlight
+    nx.draw_networkx_edges(graph, position, arrows=True, edgelist=set(graph.edges) - set(route))
+    
+    
+    # labels
+    if address:
+        nx.draw_networkx_labels(graph, position, labels=address, font_size=6)
+    else:
+        nx.draw_networkx_labels(graph, position, font_size=10)
+
+    if streetName:
+    	nx.draw_networkx_edge_labels(graph, position, edge_labels=streetName, font_size=6)
+    elif weight:
+    	nx.draw_networkx_edge_labels(graph, position, edge_labels=weight, font_size=6)
+
+
+    plt.show()
+
